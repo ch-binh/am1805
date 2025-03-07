d531393b4ae2c494f7971432eef3f641e54dc372
BinhPham-ACD <binh.pham@austincircuit.com>
2025-03-07 13:59:15 +0700

    update repo

--- inc/am1805_i2c.h
+++ inc/am1805_i2c.h
@@ -3,6 +3,16 @@
 
 #include <stdint.h>
 
+/*
+void am1805_com_init(void)
+{
+    am1805_i2c_ops_t ops = {
+        .i2c_write = hal_i2c_write_reg,
+        .i2c_read = hal_i2c_read_reg};
+    am1805_set_i2c_spec(&ops);
+}
+*/
+
 typedef struct
 {
     void (*i2c_write)(uint8_t reg, uint8_t *data, uint8_t size);
